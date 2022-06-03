9744e602ec703914b5d2055009d68e040cbd8d4f
Lethabela <tlouselolo56@gmail.com>
2022-06-03 11:20:10 -0700

    pUTS nAME

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,31 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
+ * main - Entry poiny
  *
- *  * main - return the integal of the function
- *
- *   *
- *
- *    * Description:use to print "programming is like building a mutilingual puzzle"
- *
- *     *
- *
- *      * Return: value of 0
- *
- *       *
- *
- *        */
-
-
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
