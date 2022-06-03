e18a8317195519d5dd18918b0403429b2e3e7854
Lethabela <tlouselolo56@gmail.com>
2022-06-03 11:09:02 -0700

    Puts Name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+
+
+/**
+ *
+ *  * main - return the integal of the function
+ *
+ *   *
+ *
+ *    * Description:use to print "programming is like building a mutilingual puzzle"
+ *
+ *     *
+ *
+ *      * Return: value of 0
+ *
+ *       *
+ *
+ *        */
+
+
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
