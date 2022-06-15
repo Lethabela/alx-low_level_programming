fa425d6a88a2ed42c477e1472856776ff549bd97
Lethabela <tlouselolo56@gmail.com>
2022-06-14 18:47:53 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98- reset all variables to 98
+ * @n: argument to the function
+ * Description: print results
+ * Return: return void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
