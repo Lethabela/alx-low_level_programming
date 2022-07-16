62a400a5407615899572fb7721b7a80686b47f4a
Lethabela <tlouselolo56@gmail.com>
2022-07-16 08:42:43 -0700

    updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,32 +1,27 @@
 #include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * print_list -print out the values of a linkec list
  * @h: pointer to the first linked list
+ *
  * Description: print the required value
+ *
  * Return: return the inter value
+ *
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	const list_t *ptr = NULL;
+	const list_t *tmp;
+	unsigned int i;
 
-	if (h == NULL)
-		return (0);
-	ptr = h;
-	while (ptr != NULL)
+	tmp = h;
+	for (i = 0; tmp; i++)
 	{
-		if (ptr->str == NULL)
-		{
-			printf("[%u] %s\n", 0, "(nil)");
-			free(ptr->str);
-		} else
-		{
-			print("[%u] %s\n", ptr->len, ptr->str);
-		}
-		ptr = ptr->next;
-		count++;
+		printf("[%u] %s\n", tmp->len, tmp->str);
+		tmp = tmp->next;
 	}
-	return (count);
+	return (i);
+
 }
