e4ef9e988128248dbff27f681eac2b2b70501c8a
Lethabela <tlouselolo56@gmail.com>
2022-07-19 18:35:15 -0700

    I am so happy

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,17 +1,12 @@
 #include "lists.h"
-
 #include <stdlib.h>
 
 /**
-
  * pop_listint - pops off the head of the list and returns its contents
-
  * @head: head of the list
-
+ *
  * Return: contents of head
-
  */
-
 int pop_listint(listint_t **head)
 
 {
