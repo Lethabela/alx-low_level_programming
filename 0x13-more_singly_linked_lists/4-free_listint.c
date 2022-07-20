0d56bb6821e7aef1cb678a734db2499cf306d20a
Lethabela <tlouselolo56@gmail.com>
2022-07-19 19:23:57 -0700

    updates

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,13 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stddef.h>
+
 /**
  * free_listint - define function
  * @head: describe argument
  *
  * Return: void
  */
-
 void free_listint(listint_t *head)
-
 {
 	listint_t *next;
 
