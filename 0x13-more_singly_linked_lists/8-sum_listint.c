c04fe5b98a99c1199755bc8a0db441daada1cc7d
Lethabela <tlouselolo56@gmail.com>
2022-07-19 18:43:48 -0700

    Grace

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,13 +1,10 @@
 #include "lists.h"
 
 /**
-
  * sum_listint - sums all elements of a list
-
  * @head: pointer to the head of the list
-
+ *
  * Return: sum of all elements
-
  */
 
 int sum_listint(listint_t *head)
