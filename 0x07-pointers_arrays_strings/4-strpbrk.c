c21f5ba64d723a99dc2f4b1b583d2f1743bacf6e
Lethabela <tlouselolo56@gmail.com>
2022-07-28 15:43:24 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,43 +1,24 @@
-trpbrk - string from set
-
+/**
+ * _strpbrk - string from set
  * @s: source
+ * @accept: set to find
  *
- *  * @accept: set to find
- *
- *   * Return: pointer
- *
- *    */
-
- char *_strpbrk(char *s, char *accept)
-
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
 {
+	char *ps = s;
+	char *pa = accept;
 
-		char *ps = s;
-
-			char *pa = accept;
-
-
-
-				while (*ps)
-
-						{
-
-									do {
-
-													if (*ps == *pa)
-
-																		return (ps);
-
-																pa++;
-
-																		} while (*pa);
-
-											ps++;
-
-											pa = accept;
-
-												}
-
-					return (((void *) 0));
-
+	while (*ps)
+	{
+		do {
+			if (*ps == *pa)
+				return (ps);
+			pa++;
+		} while (*pa);
+		ps++;
+		pa = accept;
+	}
+	return (((void *) 0));
 }
