8ca94be3dae34371958f7a3949d63025d2d373d2
Lethabela <tlouselolo56@gmail.com>
2022-07-28 15:26:15 -0700

    betty

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,35 +1,19 @@
 /**
+ * _memcpy - copy memory
+ * @dest: pointer to destiny
+ * @src: source string
+ * @n: n bytes
  *
- *  * _memcpy - copy memory
- *
- *   * @dest: pointer to destiny
- *
- *    * @src: source string
- *
- *     * @n: n bytes
- *
- *      * Return: pointer to dest
- *
- *       */
-
+ * Return: pointer to dest
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
-
 {
-
 		unsigned int i = 0;
 
-
-
 			while (i != n)
-
-					{
-
-								dest[i] = src[i];
-
-										i++;
-
-											}
-
-				return (dest);
-
+			{
+				dest[i] = src[i];
+				i++;
+			}
+			return (dest);
 }
