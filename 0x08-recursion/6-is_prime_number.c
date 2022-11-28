55ec81a41500d218db2c2009a1fc3f9a4ae76c63
Tlou Selolo <tlouselolo56@gmail.com>
2022-11-27 16:41:05 -0800

    recurssions

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * is_prime_number - determine if a number is a prime number
+ * @n: int number
+ * Return: 1 if prime, 0 otherwise
+ */
+
+int is_prime_number(int n);
+{
+	int prime_number(int divider, intn);
+	
+	int divider = 1;
+
+	if (n < 2)
+	{
+		return (0);
+	}
+	if (n % n == 0 || n % 1 == 0)
+	{
+		if (prime_number(divider, n) != 0)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
